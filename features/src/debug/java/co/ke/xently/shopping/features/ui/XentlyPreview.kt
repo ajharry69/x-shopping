2bc10d30c85bbb0c8ebf233e2168ea61bedf881e
Orinda Harrison <harrison@regulusweb.com>
2022-10-24 23:52:35 +0300

    Name previews

--- features/src/debug/java/co/ke/xently/shopping/features/ui/XentlyPreview.kt
+++ features/src/debug/java/co/ke/xently/shopping/features/ui/XentlyPreview.kt
@@ -7,21 +7,25 @@ import androidx.compose.ui.tooling.preview.Preview
 @Preview(
     showBackground = true,
     group = "theme",
+    name = "Light theme",
 )
 @Preview(
     showBackground = true,
     group = "theme",
+    name = "Dark theme",
     uiMode = Configuration.UI_MODE_NIGHT_YES,
 )
 @Preview(
     showBackground = true,
     group = "device",
     device = Devices.TABLET,
+    name = "Tablet light theme",
 )
 @Preview(
     showBackground = true,
     group = "device",
     device = Devices.TABLET,
+    name = "Tablet dark theme",
     uiMode = Configuration.UI_MODE_NIGHT_YES,
 )
 annotation class XentlyPreview
